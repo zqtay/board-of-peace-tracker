0d0d11b0f4da6e3059e83b0c38514501b962cd54
zqtay <zqtay96@gmail.com>
2026-01-22 23:59:24 +0800

    Add state geojson

--- src/services/data/index.ts
+++ src/services/data/index.ts
@@ -1,11 +1,20 @@
 import type { MemberListData } from "./types";
+import memberListJson from "./data.json?url";
+import stateGeoJson from "./state.geojson?url";
 
 const getMemberList = async (): Promise<MemberListData> => {
-  const response = await fetch("./data.json");
+  const response = await fetch(memberListJson);
+  const data = await response.json();
+  return data;
+};
+
+const getStateGeoJson = async (): Promise<any> => {
+  const response = await fetch(stateGeoJson);
   const data = await response.json();
   return data;
 };
 
 export const DataService = {
-  getMemberList
+  getMemberList,
+  getStateGeoJson,
 };
