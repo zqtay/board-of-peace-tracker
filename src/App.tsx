ebd667f933a5b1f4dfb2753dd60626055946da1b
zqtay <zqtay96@gmail.com>
2026-01-23 14:10:25 +0800

    Add clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import './App.css'
 import Map from "./components/map";
+import Clarity from '@microsoft/clarity';
+
+const clarityProjectId = import.meta.env.VITE_MICROSOFT_CLARITY_PROJECT_ID;
+Clarity.init(clarityProjectId);
 
 function App() {
 
